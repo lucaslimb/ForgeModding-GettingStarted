07c14301adbb6210a8cf7b9e2837bb9ddd8b260a
Lucas Lima <lucasdelimabzr@gmail.com>
2024-11-05 15:19:00 -0300

    New ore added

--- src/main/java/com/limb/firstmod/FirstMod.java
+++ src/main/java/com/limb/firstmod/FirstMod.java
@@ -1,6 +1,8 @@
 package com.limb.firstmod;
 
+import com.limb.firstmod.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -30,6 +32,8 @@ public class FirstMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -45,7 +49,10 @@ public class FirstMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.LUQUITA);
+            event.accept(ModItems.RAW_LUQUITA);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/limb/firstmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package com.limb.firstmod.item;
+
+import com.limb.firstmod.FirstMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
+
+    public static final RegistryObject<Item> LUQUITA = ITEMS.register("luquita", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_LUQUITA = ITEMS.register("raw_luquita", () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
